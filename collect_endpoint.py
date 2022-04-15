8fa828824cc8b9ca6237b174eb794cb68b83151d
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-04-15 15:43:18 +0200

    adding comments to script

--- collect_endpoint.py
+++ collect_endpoint.py
@@ -31,3 +31,4 @@ table.field_names = ['IP Address','MAC Address',"VLAN","Tenant"]
 for row in data:
     table.add_row([row['hostingServer'],row['mac'],row["encap"],row['dn'].split("/")[1]])
 print(table)
+# comment from Local repo
