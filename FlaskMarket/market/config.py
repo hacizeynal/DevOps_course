a9bcdbbca10a54c625dd956bd6a718f525619b98
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-05-25 17:44:48 +0200

    dockerize flask market v1.1

---
+++ FlaskMarket/market/config.py
@@ -0,0 +1,9 @@
+DEV_DB = "sqlite:///market.db"
+
+pg_user = "zhajili"
+pg_pass = "Krakow123"
+pg_db = "market"
+pg_host = "localhost"
+pg_port = 5432
+
+PROD_DB = f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}'
