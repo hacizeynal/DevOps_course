c31825a7e9d9fce91af7a0f453f68c657d81955f
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-06-01 11:39:40 +0200

    finalize dockerize Flask App with DB

--- FlaskMarket/web/market/config.py
+++ FlaskMarket/web/market/config.py
@@ -3,7 +3,7 @@ DEV_DB = "sqlite:///market.db"
 pg_user = "zhajili"
 pg_pass = "Krakow123"
 pg_db = "market"
-pg_host = "localhost"
+pg_host = "db"
 pg_port = 5432
 
 PROD_DB = f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}'
