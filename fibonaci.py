3c1a915b42726cbeaa49c5ac20f6a2a2569bbb79
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-06-09 14:35:34 +0200

    scripts for interview

--- fibonaci.py
+++ fibonaci.py
@@ -13,16 +13,35 @@
 """
 
 
-def fibo(n):
-    a = 0
-    b = 1
-    # print(a)
-    # print(b)
+# def fibo(n):
+#     global c
+#     a = 0
+#     b = 1
+#     # print(a)
+#     # print(b)
+#
+#     for i in range(2, n):
+#         c = a + b
+#         a = b
+#         b = c
+#     return c
+#
+#
+# print(fibo(9))
 
-    for i in range(2, n):
-        c = a + b
-        a = b
-        b = c
-    return c
 
-print(fibo(9))
+def fib(number_of_terms):
+    counter = 0
+    first = 0
+    second = 1
+    temp = 0
+
+    while counter <= number_of_terms:
+        print(first)
+        temp = first + second
+        first = second
+        second = temp
+        counter = counter + 1
+# Driver Code
+fib(9)
+
