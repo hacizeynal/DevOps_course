222ca52aeb893e7e759263f8fd80850fb45b9c23
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-09-04 02:48:46 +0200

    new branch for ansible-jenkins integration

--- FlaskMarket/web/run.py
+++
@@ -1,6 +0,0 @@
-from market import app
-import os
-# docker run -d -p 5002:5000 -e DEBUG=1 --name web_app <image_name>
-# Checks if the run.py file has executed directly and not imported
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=os.environ.get("DEBUG") == "1")
